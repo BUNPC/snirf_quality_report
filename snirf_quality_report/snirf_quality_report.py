dead3a2e100c0a474692e301053c413fa7a6de42
Sreekanth kura <skura@bu.edu>
2022-05-17 10:35:52 -0400

    Update snirf_quality_report.py

--- snirf_quality_report/snirf_quality_report.py
+++ snirf_quality_report/snirf_quality_report.py
@@ -179,9 +179,9 @@ class snirf_quality_report():
 				else:
 					report_dir = os.path.join('',*all_dirs[:return_idx],'derivatives',all_dirs[return_idx])
 					
-				report_dir = os.path.abspath(report_dir)
+				report_dir = Path('/'+report_dir)
 
-				print(report_dir)
+				# print(report_dir)
 
 				if not os.path.isdir(report_dir):
 					os.makedirs(report_dir)
